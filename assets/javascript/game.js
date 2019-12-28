414d49ebfa6c17e368833b455e5c6d92f190c2c0
Mangenese <blakemangen20@gmail.com>
2019-12-28 16:24:40 -0700

    added psuedo code on j

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// create a function to start the game 
+// create 3 or 4 characters
+// choose one of the characters to play as
+// choose the enemy you want to fight 
+// all other characters should be moved to a different area
+// create a place for life points, atk power, and or counter atk power
+// once a char is beat they get more atk pwr.
+// choose the next enemy to fight
+// game ends if player defeats all characters or  player dies
+// create  a fun to create atks  
