342cbc4bb006d4452b8009d6852196cb3cd806f4
Mangenese <blakemangen20@gmail.com>
2020-01-02 17:36:12 -0700

    added the code for the game to run, all if, else statements for the js. For the html I fixed some of the classes and ids

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,18 +7,99 @@
 // on a new game random the buttons
 
 //setting global variables
-var win = 0;
-var loss = 0;
-
+var wins = 0;
+var losses = 0;
+var blueNum = 0;
+var redNum = 0;
+var greenNum = 0;
+var yellowNum = 0;
+var randomNum = 0;
+var total = 0;
 // random number generator for the computer guess
 
-var minCompNum = 19;
-var maxCompNum = 120;
+function randomNumGen() {
+    total = 0;
 
-var randomNumber = randomNumberFromRange(minCompNum, maxCompNum);
+    $("#total").text(total);
+    //The random number gets generated for the user to match
+    randomNum = Math.floor(Math.random() * 120) + 19;
 
-function randomNumberFromRange(min,max)
-{ return Math.floor(Math.random()*(max-min+1)+min);
+    //generate a random number for the rupees
+    blueNum = Math.floor(Math.random() * 12) + 1;
+    greenNum = Math.floor(Math.random() * 12) + 1 ;
+    redNum = Math.floor(Math.random() * 12) + 1;
+    yellowNum = Math.floor(Math.random() * 12) + 1; 
+    console.log(blueNum, greenNum, redNum, yellowNum);
+
+
+    $("#rndNum").text(randomNum);
+
 }
-    winRndNum.text(randomNumber);
-console.log(randomNumber);
+
+randomNumGen();
+
+
+// To start the game the game needs to display the random number
+// If the total === randomNum then you win
+// If the total > randomNum you lose!
+
+$(".blue").on("click", function () {
+
+    if(total === randomNum){
+        wins++;
+        $("#win").text(wins);
+        randomNumGen();
+    } if(total > randomNum) {
+        losses++;
+        $("#losses").text(losses);
+        randomNumGen();
+
+    } 
+    else {total = total + blueNum;
+    $("#total").text(total); }
+});
+
+$(".green").on("click", function() {
+    if (total === randomNum){
+        wins++;
+        $("#win").text(wins);
+        randomNumGen()
+    } else  if (total > randomNum){
+        losses++;
+        $("#losses").text(losses);
+        randomNumGen();
+    } else {
+        total = total + greenNum;
+        $("#total").text(total); 
+    }
+});
+
+$(".red").on("click", function() {
+    if (total === randomNum){
+        wins++;
+        $("#win").text(wins);
+        randomNumGen()
+    } else  if (total > randomNum){
+        losses++;
+        $("#losses").text(losses);
+        randomNumGen();
+    } else {
+        total = total + redNum;
+        $("#total").text(total); 
+    }
+});
+
+$(".yellow").on("click", function() {
+    if (total === randomNum){
+        wins++;
+        $("#win").text(wins);
+        randomNumGen()
+    } else  if (total > randomNum){
+        losses++;
+        $("#losses").text(losses);
+        randomNumGen();
+    } else {
+        total = total + yellowNum;
+        $("#total").text(total); 
+    }
+});
